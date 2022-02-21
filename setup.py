088e8d753dbfb26cf8ae426fc47ced05b9300571
sadnub <josh@torchlake.com>
2022-02-21 12:05:21 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='meshctrl',
-    version='0.1.8',    
+    version='0.1.9',    
     description='Python port of MeshCentral\'s Meshctrl.js program',
     long_description=long_description,
     long_description_content_type="text/markdown",
